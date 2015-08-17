c909ec010ce1454fd48d10d26c89dc4f8485eb52
MoskalenkoViktor <ss2015@ws-2k-114-11n.ccam.cmc.unn.net>
2015-08-17 18:31:23 +0300

    123
    123

--- src/filters_factory.cpp
+++ src/filters_factory.cpp
@@ -3,6 +3,7 @@
 Filters* createFiltersOpenCV();
 // Declare your implementation here
 // Filters* createFiltersYourName();
+Filters* createFiltersMoskalenko();
 
 Filters* createFilters(FILTERS_IMPLEMENTATIONS impl)
 {
@@ -12,6 +13,8 @@ Filters* createFilters(FILTERS_IMPLEMENTATIONS impl)
         // Add case for your implementation
         // case YOUR_NAME:
         //     return createFiltersYourName();
+		case Moskalenko:
+            return createFiltersMoskalenko();
         default:
             return 0;
     }
